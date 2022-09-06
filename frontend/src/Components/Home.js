0ada24e408ec3835e78f419e0fa610355bc976c3
Samuel Dorismond <samuel.dorismond@yahoo.com>
2022-09-06 16:42:42 +0200

    ok

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,4 +1,5 @@
 import MyTask from './MyTask'
+import styled from 'styled-components'
 
 export default function Home(props){
     if (!!document.cookie) {
@@ -8,10 +9,28 @@ export default function Home(props){
     } else {
         return (
             <div>
-                <h1>Home</h1>
-                <h3>Description du site web</h3>
-                <p>Texte du site web</p>
+                <Content>
+                    <div>
+                        <h1>Bienvenue sur Matrice - Eisenhower !</h1>
+                        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.</p>
+                    </div>
+                </Content>
             </div>
         )   
     }
 }
+
+const Content = styled.section`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: calc(100vh - 200px);
+
+    & > div{
+        width: 70vw;
+    }
+`
